480ada326aaa28813fb048bc6abd43ab0c00dfe8
estiakahamed01 <tasin220116@diit.edu.bd>
2024-09-20 17:08:21 +0600

    Add Money Done

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@
  * S1: add event handler
  * prevent page reload after form submit
  * S2:get money to be added to the acoount balance
+ * get the pin number
+ * S3: verify the pin number
  */
 
 
@@ -15,9 +17,29 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
     //STEP 2:get money to be added to the acoount
     const addMoneyInput = document.getElementById('input-add-money').value;
-    console.log(addMoneyInput)
 
     //get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput);
+
+    //Step 3: verify ppin number
+    if(pinNumberInput === '1234'){
+
+        //Step 4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+
+        //Step 5 : Add addMoneyInput with balance
+
+        const addMoneyNumber = parseFloat(addMoneyInput)
+        const balanceNumber = parseFloat(balance)
+
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance)
+
+        //step 6: update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText= newBalance;
+        
+    }
+    else{
+        alert('Failed!! Please Try Again')
+    }
 })
